cd0a707f4c27dc45fd124af9d24d14e71d467ac8
davidemastruzzo <mastruzzodavide@gmail.com>
2019-05-14 09:09:10 +0200

    Custom Cors Filter fix

--- guestbook/src/main/java/ch/bbw/guestbook/security/CorsFilter.java
+++ guestbook/src/main/java/ch/bbw/guestbook/security/CorsFilter.java
@@ -20,7 +20,7 @@ public class CorsFilter implements Filter {
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods",
                 "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
